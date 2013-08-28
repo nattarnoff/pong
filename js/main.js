c54b92fe31a64e10071dd748449336e4f81eff6e
Tarnoff <glt007@00000433765mac.corporate.amfam.com>
2013-08-28 13:17:07 -0500

    keypress captured. S/X move left up/down, K/M move right up/down

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ pong.setBoard = function(){
 	$b = $('#board')
 	var w = $b.width();
 	var h = $b.height();
-	pong.paddle($b, "right", w, h);
-	pong.paddle($b, "left", w, h);
-	pong.ball($b, w, h);
+	var right = pong.paddle($b, "right", w, h);
+	var left = pong.paddle($b, "left", w, h);
+	var ball = pong.ball($b, w, h);
+	return { 
+		right : this.right,
+		left : this.left,
+		ball : this.ball
+	}
 }
 pong.paddle = function(canvas, side, w, h){
 	var w = parseInt(w);
@@ -39,5 +44,19 @@ pong.ball = function(canvas, w,h){
 }
 $(function(){
 	pong.setBoard();
-	// console.log($('#board'));
+	$(document).keypress(function(e){
+		if(e.which === 107){
+			console.log("right up");
+			// pong.board.right
+		}
+		if(e.which === 109){
+			console.log("right down");
+		}
+		if(e.which === 115){
+			console.log("left up");
+		}
+		if(e.which === 120){
+			console.log("left down");
+		}
+	});
 })
